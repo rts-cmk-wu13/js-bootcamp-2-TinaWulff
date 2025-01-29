734ad1e7e92f980d55667da682b1821ffac47bf6
Tina Wulff <tina@tinawulff.dk>
2025-01-29 08:54:05 +0100

    opgave 2 niv 1

---
+++ Opgaver/Niveau1/01/02.js
@@ -0,0 +1,17 @@
+/*function add (x, y) {
+    return x + y;
+}
+
+let outerResult = add(2,3);
+
+console.log(outerResult)
+*/
+
+
+function divide (a, b) {
+    return a / b;
+}
+
+let resultat = divide(4,3);
+
+console.log(resultat)
